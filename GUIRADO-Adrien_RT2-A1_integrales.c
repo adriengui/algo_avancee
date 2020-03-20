879dea0d62e70c6c1f5f2a3a9b705226e20eb329
adriengui <55453387+adriengui@users.noreply.github.com>
2020-03-20 23:50:21 +0100

    Add files via upload

--- GUIRADO-Adrien_RT2-A1_integrales.c
+++ GUIRADO-Adrien_RT2-A1_integrales.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(double n){
-    return(2*pow(n,5)+3*pow(n,3)-4*pow(n,2)+1);
+double f(double x){
+    // return(2*pow(x,5)+3*pow(x,3)-4*pow(x,2)+1);
+    return(pow(x,3)-5*x);
 }
 
 double rec(double deb, double fin, double pas){
@@ -19,12 +20,27 @@ double rec(double deb, double fin, double pas){
     return somme;
 }
 
+double tra(double deb, double fin, double pas){
+    double n2=(fin-deb)/pas,a=deb,somme=0;
+    int n=n2;
+    printf("%d\n\n",n);
+    double tab[n];
+    for(int x=0;x<n;x++){
+        tab[x]=pas*f(a)+0.5*pas*(f(a+pas)-f(a));
+        a+=pas;
+    }
+    for(int y=0;y<n;y++)
+        somme+=tab[y];
+    return somme;
+}
+
 int main(){
 
     printf("GUIRADO Adrien - RT2 A1\n\n");
 
-printf("%g\n\n",rec(-2,2,0.00005));
-
+    double d=-3,f=5,p=0.00005;
+printf("%g\n\n",rec(d,f,p));
+printf("\n\n%lf\n\n",tra(d,f,p));
 
 
     return 0;
