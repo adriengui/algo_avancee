2daed10aa57a2637f973109dfa2416dc54209bb7
adriengui <55453387+adriengui@users.noreply.github.com>
2020-04-14 19:35:31 +0200

    Update GUIRADO-Adrien_RT2-A1_matrices.c

--- GUIRADO-Adrien_RT2-A1_matrices.c
+++ GUIRADO-Adrien_RT2-A1_matrices.c
@@ -5,9 +5,8 @@
 
 double** create_tab(int m,int n){
     double** T;
-    int i;
     T=(double**) malloc(m*sizeof(double*));
-    for(i=0;i<m;i++)
+    for(int i=0;i<m;i++)
         T[i]=(double*) malloc(n*sizeof(double));
     for(int x=0;x<m;x++)
         for(int y=0;y<n;y++)
@@ -32,7 +31,7 @@ void aff(double** mat, int m, int n){
 /* Erreurs :
 
     0 : OK
-    1 : Erreur de dimensions (les matrices n'ont pas les mêmes dimensions)
+    1 : Erreur de dimensions (les matrices n'ont pas les mÃªmes dimensions)
 
 */
 int addi(double** mat1, int m1, int n1,double** mat2, int m2, int n2, double** res){
@@ -49,7 +48,7 @@ int addi(double** mat1, int m1, int n1,double** mat2, int m2, int n2, double** r
 /* Erreurs :
 
     0 : OK
-    1 : Erreur de dimensions (la matrice n'est pas carrée)
+    1 : Erreur de dimensions (la matrice n'est pas carrÃ©e)
 
 */
 int trace(double** mat, int m, int n, double *res){
@@ -94,7 +93,7 @@ int sous_mat(double** mat, int m, int n, int ligne,int colonne, double** T){
 /* Erreurs :
 
     0 : OK
-    1 : Erreur de dimensions (la matrice n'est pas carrée)
+    1 : Erreur de dimensions (la matrice n'est pas carrÃ©e)
 
 */
 int det(double** mat, int m, int n, double* res){
@@ -124,7 +123,7 @@ int det(double** mat, int m, int n, double* res){
 /* Erreurs :
 
     0 : OK
-    1 : Erreur de dimensions (le nombre de colonnes de la matrice A est différent du nombre de lignes de la matrice B)
+    1 : Erreur de dimensions (le nombre de colonnes de la matrice A est diffÃ©rent du nombre de lignes de la matrice B)
 
 */
 int mul(double** mat1, int m1, int n1,double** mat2, int m2, int n2, double** res){
@@ -150,100 +149,141 @@ void remp(double** a, int m, int n, int deb, int fin){
             a[x][y]=rand()%(fin+1-deb)+deb;
 }
 
+void saisie(int *test, double **a, int m, int n){
+    while(*test != 0 && *test != 1){
+        printf("\nMatrice aleatoire ? (1=oui et 0=non) : ");
+        scanf("%d",&*test);
+    }
+    if(*test==1)
+        remp(a,m,n,-10,9);
+    else{
+        for(int y=0;y<m;y++){
+            for(int z=0;z<n;z++){
+               printf("m%d n%d : ",y+1,z+1);
+                scanf("%lf",&a[y][z]);
+            }
+        }
+    }
+}
+
 int main(){
 
     srand(time(NULL));
 
     printf("GUIRADO Adrien - RT2 A1\n\n");
 
-    int aa1=11,aa2=9;
-    printf("Affichage matrice (%dx%d) :\n",aa1,aa2);
-    double** a=create_tab(aa1,aa2);
-    remp(a,aa1,aa2,-10,9);
-    aff(a,aa1,aa2);
-    delete_tab(a,aa1);
+    printf("Operations sur les matrices\n\n1 : Affichage matrice\n2 : Somme matrices\n3 : Produit matrices\n4 : Trace matrice\n5 : Determinant matrice\n\n");
 
-    int a11=7,a12=8,a21=7,a22=8,b03=0;
-    printf("\n\nAddition matrices :\n\nMatrice A (%dx%d) :\n",a11,a12);
-    double** b=create_tab(a11,a12);
-    double** c=create_tab(a21,a22);
-    double** d=create_tab(a11,a12);
-    remp(b,a11,a12,-10,9);
-    remp(c,a21,a22,-10,9);
-    aff(b,a11,a12);
-    printf("\nMatrice B (%dx%d) :\n",a21,a22);
-    aff(c,a21,a22);
-    printf("\nA + B (%dx%d) :\n",a11,a12);
-    if(b03=addi(b,a11,a12,c,a21,a22,d)){
-        switch(b03){
+    int k=1;
+
+    while(k != 0){
+        printf("Operation souhaitee (0 pour quitter) : ");
+        scanf("%d",&k);
+        switch(k){
             case 1:
-                printf("Erreur de dimension\n\n\n");
+            {
+                int aa1=0,aa2=0,aff_test=2;
+                printf("Affichage matrice :\nNb lignes : ");
+                scanf("%d",&aa1);
+                printf("\nNb colonnes : ");
+                scanf("%d",&aa2);
+                double** a=create_tab(aa1,aa2);
+                saisie(&aff_test,a,aa1,aa2);
+                printf("\n");
+                aff(a,aa1,aa2);
+                delete_tab(a,aa1);
+                printf("\n\n");
                 break;
+            }
+            case 2:
+            {
+                int a11=0,add_test=2,add_test2=2;
+                printf("\n\nAddition matrices :\n\nDimension : ");
+                scanf("%d",&a11);
+                double** b=create_tab(a11,a11);
+                double** c=create_tab(a11,a11);
+                double** d=create_tab(a11,a11);
+                printf("\nSaisie matrice A :\n");
+                saisie(&add_test,b,a11,a11);
+                aff(b,a11,a11);
+                printf("\n\nSaisie matrice B :\n");
+                saisie(&add_test2,c,a11,a11);
+                aff(c,a11,a11);
+                printf("\nA + B (%dx%d) :\n",a11,a11);
+                addi(b,a11,a11,c,a11,a11,d);
+                aff(d,a11,a11);
+                delete_tab(b,a11);
+                delete_tab(c,a11);
+                delete_tab(d,a11);
+                break;
+            }
+            case 3:
+            {
+                int m11=0,m12=0,m21=0,m22=0,b02=0,mul_test=2,mul_test2=2;
+                printf("\n\nMultiplication matrices :\n\nMatrice A :\nNb lignes :");
+                scanf("%d",&m11);
+                printf("\nNb colonnes : ");
+                scanf("%d",&m12);
+                printf("\nMatrice B :\nNb lignes : ");
+                scanf("%d",&m21);
+                printf("\nNb colonnes : ");
+                scanf("%d",&m22);
+                if(m12==m21){
+                    double** e=create_tab(m11,m12);
+                    double** f=create_tab(m21,m22);
+                    double** g=create_tab(m11,m22);
+                    printf("\nSaisie matrice A :\n");
+                    saisie(&mul_test,e,m11,m12);
+                    aff(e,m11,m12);
+                    printf("\nSaisie matrice B :\n");
+                    saisie(&mul_test2,f,m21,m22);
+                    aff(f,m21,m22);
+                    printf("\nA x B (%dx%d) :\n",m11,m22);
+                    mul(e,m11,m12,f,m21,m22,g);
+                    aff(g,m11,m22);
+                    delete_tab(e,m11);
+                    delete_tab(f,m21);
+                    delete_tab(g,m11);
+                }
+                else
+                    printf("\nErreur de dimensions\n");
+
+                break;
+            }
+            case 4:
+            {
+                int t1=0,tra_test=2;
+                printf("\n\nTrace d'une matrice :\n\nDimension : ");
+                scanf("%d",&t1);
+                double** h=create_tab(t1,t1);
+                double tra=0;
+                printf("\nSaisie matrice :\n");
+                saisie(&tra_test,h,t1,t1);
+                aff(h,t1,t1);
+                trace(h,t1,t1,&tra);
+                printf("\nTrace : %g\n\n",tra);
+                delete_tab(h,t1);
+
+                break;
+            }
+            case 5:
+            {
+                int d1=00,det_test=2;
+                printf("Determinant d'une matrice :\n\nDimension :");
+                scanf("%d",&d1);
+                double** i=create_tab(d1,d1);
+                double deter=0;
+                saisie(&det_test,i,d1,d1);
+                aff(i,d1,d1);
+                det(i,d1,d1,&deter);
+                printf("\nDeterminant : %lf\n\n",deter);
+                delete_tab(i,d1);
+                printf("\n");
+
+                break;
+            }
         }
     }
-    else
-        aff(d,a11,a12);
-    delete_tab(b,a11);
-    delete_tab(c,a21);
-    delete_tab(d,a11);
-
-    int m11=9,m12=7,m21=7,m22=8,b02=0;
-    printf("\n\nMultiplication matrices :\n\nMatrice A (%dx%d) :\n",m11,m12);
-    double** e=create_tab(m11,m12);
-    double** f=create_tab(m21,m22);
-    double** g=create_tab(m11,m22);
-    remp(e,m11,m12,-10,9);
-    remp(f,m21,m22,-10,9);
-    aff(e,m11,m12);
-    printf("\nMatrice B (%dx%d) :\n",m21,m22);
-    aff(f,m21,m22);
-    printf("\nA x B (%dx%d) :\n",m11,m22);
-    if(b02=mul(e,m11,m12,f,m21,m22,g)){
-        switch(b02){
-            case 1:
-                printf("Erreur de dimension\n\n");
-                break;
-        }
-    }
-    else
-        aff(g,m11,m22);
-    delete_tab(e,m11);
-    delete_tab(f,m21);
-    delete_tab(g,m11);
-
-    int t1=12,t2=12,b01=0;
-    printf("\n\nTrace d'une matrice :\n\nMatrice (%dx%d) :\n",t1,t2);
-    double** h=create_tab(t1,t2);
-    double tra=0;
-    remp(h,t1,t2,-10,9);
-    aff(h,t1,t2);
-    if(b01=trace(h,t1,t2,&tra)){
-        switch(b01){
-            case 1:
-                printf("Erreur de dimension\n\n\n");
-                break;
-        }
-    }
-    else
-        printf("\nTrace : %g\n\n\n",tra);
-    delete_tab(h,t1);
-
-    int d1=10,d2=10,b00=0;
-    printf("Determinant d'une matrice :\n\nMatrice (%dx%d) :\n",d1,d2);
-    double** i=create_tab(d1,d2);
-    double deter=0;
-    remp(i,d1,d2,-1,2);
-    aff(i,d1,d2);
-    if(b00=det(i,d1,d2,&deter)){
-        switch(b00){
-            case 1:
-                printf("Erreur de dimension\n");
-                break;
-        }
-    }
-    else
-        printf("\nDeterminant : %g\n\n",deter);
-    delete_tab(i,d1);
 
 	return 0;
 }
