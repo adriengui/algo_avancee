6d5aa6dee79fb9cd699e8e8666b79e3adc63224c
adriengui <55453387+adriengui@users.noreply.github.com>
2020-06-14 22:26:17 +0200

    Update GUIRADO-Adrien_RT2-A1_integrales.c

--- GUIRADO-Adrien_RT2-A1_integrales.c
+++ GUIRADO-Adrien_RT2-A1_integrales.c
@@ -3,34 +3,28 @@
 
 double f(double x){
     // return(2*pow(x,5)+3*pow(x,3)-4*pow(x,2)+1);
-    return(pow(x,3)-5*x);
+    // return(pow(x,3)-5*x);
+    // return(2*pow(x,3)-5*pow(x,2)+2);
+    return(pow(x,2)*log(x+10)-20);
 }
 
 double rec(double deb, double fin, double pas){
-    double n2=(fin-deb)/pas,a=deb,somme=0;
-    int n=n2;
-    printf("%d\n\n",n);
-    double tab[n];
+    double somme=0;
+    int n=(fin-deb)/pas;
     for(int x=0;x<n;x++){
-        tab[x]=pas*f(a);
-        a+=pas;
+        somme+=pas*f(deb);
+        deb+=pas;
     }
-    for(int y=0;y<n;y++)
-        somme+=tab[y];
     return somme;
 }
 
 double tra(double deb, double fin, double pas){
-    double n2=(fin-deb)/pas,a=deb,somme=0;
-    int n=n2;
-    printf("%d\n\n",n);
-    double tab[n];
+    double somme=0;
+    int n=(fin-deb)/pas;
     for(int x=0;x<n;x++){
-        tab[x]=pas*f(a)+0.5*pas*(f(a+pas)-f(a));
-        a+=pas;
+        somme+=0.5*pas*(f(deb)+f(deb+pas));
+        deb+=pas;
     }
-    for(int y=0;y<n;y++)
-        somme+=tab[y];
     return somme;
 }
 
@@ -38,10 +32,20 @@ int main(){
 
     printf("GUIRADO Adrien - RT2 A1\n\n");
 
-    double d=-3,f=5,p=0.00005;
-printf("%g\n\n",rec(d,f,p));
-printf("\n\n%lf\n\n",tra(d,f,p));
+    double d=-5,f=7.5,p=0.5,i=211.8;
+    int n=(f-d)/p;
 
+    printf("Exemple :\n\n\
+f(x) = x^2*ln(x+10)-20\n\
+Intervalle = %g ; %g\n\
+Pas = %g\n\
+Nb d'iterations = (%g - %g)/%g = %g/%g = %d\n\n\
+Integrale calculee = %g\n\n\
+Methode des rectangles = %g\n\n\
+Pourcentage d'erreur = ((%g-%g)*100)/%g = (%g*100)/%g = %g/%g = %g pourcents\n\n\
+Methode des trapezes = %g\n\n\
+Pourcentage d'erreur = ((%g-%g)*100)/%g = (%g*100)/%g = %g/%g = %g pourcents\n\n"
+,d,f,p,f,d,p,f-d,p,n,i,rec(d,f,p),i,rec(d,f,p),i,i-rec(d,f,p),i,(i-rec(d,f,p))*100,i,((i-rec(d,f,p))*100)/i,tra(d,f,p),i,tra(d,f,p),i,i-tra(d,f,p),i,(i-tra(d,f,p))*100,i,((i-tra(d,f,p))*100)/i);
 
     return 0;
 }
