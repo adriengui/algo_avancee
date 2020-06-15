8f7836e2f858c3b223bdac619d3d029dbb7b73cd
adriengui <55453387+adriengui@users.noreply.github.com>
2020-06-15 03:40:11 +0200

    Update GUIRADO-Adrien_RT2-A1_tangentes.c

--- GUIRADO-Adrien_RT2-A1_tangentes.c
+++ GUIRADO-Adrien_RT2-A1_tangentes.c
@@ -2,33 +2,50 @@
 #include <math.h>
 
 double f(double x){
-    return(exp(x-2)*20*(x-2)+2);
+    return(20*(x-2)*exp(x-2)+2);
+    // return(-5*pow(x,2)-3*x+8);
+    // return((15/exp(x))*log(x)-0.5);
+    // return(pow(x,3)+pow(x,2)-5*x);
+    // return(sqrt(x));
 }
 
 double fp(double x){
-    return(20*x*exp(x-2)-20*exp(x-2));
+    return(20*(x-1)*exp(x-2));
+    // return(-10*x-3);
+    // return((15-15*x*log(x))/(x*exp(x)));
+    // return(3*pow(x,2)+2*x-5);
+    // return(1/(2*sqrt(x)));
+}
+
+void tang(){
+    int x=0;
+    float a,b,t;
+
+    while(1){
+        printf("x0 (200 pour quitter) : ");
+        scanf("%f",&a);
+        if(a==200)
+            break;
+        x=0;
+        while(a!=b && x<50){
+            b=a-(f(a)/fp(a));
+            t=a;
+            a=b;
+            b=t;
+            x++;
+        }
+        if(x<50)
+            printf("\n\n%d\n%lf\n\n",x,b);
+        else
+            printf("\n\nNon trouve\n\n");
+    }
 }
 
 int main(){
 
-    printf("GUIRADO Adrien - RT2 A1\n\n");
+    printf("GUIRADO Adrien - RT2 A1\n\nMethode des tangentes\n\n");
 
-    int x=0;
-    float a[50]={0},b=0;
-
-    while(1){
-        printf("x0 (200 pour quitter) : ");
-        scanf("%f",&b);
-        if(b==200)
-            break;
-        a[0]=b;
-        for(x=1;x<50;x++)
-            a[x]=a[x-1]-(f(a[x-1])/fp(a[x-1]));
-        if(a[48]==a[49])
-            printf("%g\n\n",a[49]);
-        else
-            printf("Aucune solution trouvee pour le x0 donne\n\n");
-    }
+    tang();
 
     return 0;
 }
